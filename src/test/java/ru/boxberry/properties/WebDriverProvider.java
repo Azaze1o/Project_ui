bc0eeca300c1ae123f8ed5816ef4337392dc7e81
Илья <gagarinila@yandex.ru>
2023-01-18 20:56:02 +0500

    ui tests + config

--- src/test/java/ru/boxberry/properties/WebDriverProvider.java
+++ src/test/java/ru/boxberry/properties/WebDriverProvider.java
@@ -8,16 +8,16 @@ public class WebDriverProvider {
 
     public static void configure() {
 
-        Configuration.baseUrl = WebDriverProvider.config.browserSize();
+        Configuration.browserSize = WebDriverProvider.config.browserSize();
         Configuration.browser = WebDriverProvider.config.browser();
         Configuration.browserVersion = WebDriverProvider.config.browserVersion();
 
-        Configuration.remote="https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote="https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-//        String remoteUrl = WebDriverProvider.config.remote();
-//        if (remoteUrl != null) {
-//            Configuration.remote = remoteUrl;
-//        }
+        String remoteUrl = WebDriverProvider.config.remote();
+        if (remoteUrl != null) {
+            Configuration.remote = remoteUrl;
+        }
 
 
     }
